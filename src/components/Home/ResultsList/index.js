16a1a15722f96bad09858631430754d8cd6deb2e
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-02-12 21:36:18 +0530

    Api

--- src/components/Home/ResultsList/index.js
+++ src/components/Home/ResultsList/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import './style.css';
 
-const  ResultList = () => {
-  return (<h1>ResultList</h1>)
-}
+const ResultList = ({ images }) => (
+    <div className='resultList-container'>
+      {images.map(image => {
+        <img key={image.id} src={image.userImageURL} alt="img" />
+        {/* <h1>{image.id}, </h1> */}
+      })}
+    </div>)
 
 export default ResultList
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,79 +6,103 @@ import Loader from '../../components/common/Loader'
 import EmptyViewComponent from '../../components/common/EmptyListComponent'
 import ResultList from '../../components/Home/ResultsList'
 import MenuItemsList from '../../components/Home/MenuItems'
-import axios from 'react';
 import { pixabayAPI } from '../../config/data'
-
+import axios from 'axios';
 const Home = () => {
 
   const [showMenu, setShowMenu] = useState(true);
   const [loading, setLoading] = useState(false);
-  const [imageList, setImageList] = useState([]);
   const [searchInput, setSearchInput] = useState("");
   const [searchResultsCount, setSearchResultsCount] = useState(20);
+  const [imageList, setImageList] = useState([]);
+  // const [addImages, setAddImages] = useState([]);
 
-  const searchResults = (search) => {
+
+  const searchResults = async (search) => {
+
+    const API = `${pixabayAPI.url}?key=${pixabayAPI.key}&q=${search}&image_type=photo&per_page=${searchResultsCount}&safeSearch=true`; 
 
     setLoading(true);
-    setShowMenu(false)
-
-    axios.get(`${pixabayAPI.url}?key=${pixabayAPI.key}&q=${search}&image_type=photo&per_page=${searchResultsCount}&safeSearch=true`).then(res=>{
+    setShowMenu(false);
+    axios.get(API).then((res)=>{
       setImageList(res.data.hits);
+      console.log(res.data.hits);
+      console.log(imageList);
       setLoading(false);
     }).catch((err) => console.log(err));
-  }
-
-  const handleFormSubmit = (e) =>{
-    e.preventDefault();
-    searchResults(searchInput);
-  }
-
-  const handleHomeClick =()=>{
-    setSearchInput('');
-    setImageList([]);
-    setShowMenu(true);
-  }
-
-  const handleMenuItemClicked = (e) =>{
-    const searchFor = e.target.id;
-    console.log(searchFor);
-    setSearchInput(searchFor);
-    searchResults(searchFor)
-  }
 
 
-  return (
-    <>
-      {/* header */}
-      <Header handleHomeClick={handleHomeClick}/>
-
-      {/* menu */}
-      <main>
-        {showMenu && <Menu handleFormSubmit={handleFormSubmit} handleInputChange={e=>setSearchInput(e.target.value)} searchInputValue={searchInput}/>}
-
-
-        {/* people also seacrh for */}
-        {showMenu && <MenuItemsList handleMenuItemClicked={handleMenuItemClicked}/>}
+    // axios.get(API)
+    // .then((response) => {
+    //   // setImageList(response.data.hits);
+    //   setAddImages(response.data.hits);
+    //   console.log(response.data.hits);
+    //   console.log("My Images", addImages)
+    //   setLoading(false);
+    // })
+    // .catch(error => console.log(`Error: ${error}`));
 
 
-        {/* Loader */}
-        {loading && <Loader />}
+    // const response = await fetch(`${pixabayAPI.url}?key=${pixabayAPI.key}&q=${search}&image_type=photo&per_page=${searchResultsCount}&safeSearch=true`);
+    // const data = await response.json();
+    // console.log(data?.hits);
+    // setImageList(data?.hits);
+    // console.log(imageList)
+    // setLoading(false);
+}
+
+const handleFormSubmit = (e) => {
+  e.preventDefault();
+  searchResults(searchInput);
+}
+
+const handleHomeClick = () => {
+  setSearchInput('');
+  setImageList([]);
+  // setAddImages([]);
+  setShowMenu(true);
+}
+
+const handleMenuItemClicked = (e) => {
+  const searchFor = e.target.id;
+  console.log(searchFor);
+  setSearchInput(searchFor);
+  searchResults(searchFor)
+}
+
+
+return (
+  <>
+    {/* header */}
+    <Header handleHomeClick={handleHomeClick} />
+
+    {/* menu */}
+    <main>
+      {showMenu && <Menu handleFormSubmit={handleFormSubmit} handleInputChange={e => setSearchInput(e.target.value)} searchInputValue={searchInput} />}
+
+
+      {/* people also seacrh for */}
+      {showMenu && <MenuItemsList handleMenuItemClicked={handleMenuItemClicked} />}
+
+
+      {/* Loader */}
+      {loading && <Loader />}
 
 
 
-        {/* ImageList */}
-        {!showMenu && !loading && !imageList.length ? (<EmptyViewComponent />) : null }
+      {/* ImageList */}
+      {!showMenu && !loading && !imageList.length ? (<EmptyViewComponent />) : null}
 
 
-        {/* result List */}
-        {!showMenu && !loading && imageList.length ? (<ResultList images={imageList} />) : null }
-      </main>
+      {/* result List */}
+      {!showMenu && !loading && imageList.length ? (<ResultList images={imageList} />) : null}
+    </main>
 
 
-      {/* footer */}
-      <Footer />
-    </>
-  )
+    {/* footer */}
+    <Footer />
+  </>
+)
 }
 
 export default Home
